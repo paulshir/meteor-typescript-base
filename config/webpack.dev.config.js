36dfca3ec056e4760345cdc40133498464b2f9bb
Paul Shirley <paulshir@gmail.com>
2015-10-08 20:06:55 +0100

    Mainly Commit Updates

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -3,26 +3,28 @@ var webpack = require('webpack');
 
 module.exports = {
   context: path.join(__dirname, '..'),
-  devtool: 'inline-source-map',
+  devtool: 'eval',
   entry: {
     client: './src/client/client.tsx',
-    server: './src/server/server.ts'
+    server: './src/server/server.ts',
+    clientlibs: ['react', 'react-dom', 'normalize-css']
   },
   module: {
     loaders: [
-      { test: /\.tsx?$/, loader: 'expose?app!ts-loader'},
+      { test: /\.tsx?$/, loader: 'expose?app!babel!ts-loader'},
       { test: /\.html$/, loader: 'file?name=client/[name].html'},
       { test: /\.css$/, loader: 'style!css!cssnext' },
       { test: /\.less$/, loader: 'style!css!less' }
     ]
   },
   output: {
-    filename: '[name]/[name].js',
-    sourceMapFilename: '[name]/[name].js.map',
+    filename: '[name]/main.js',
+    sourceMapFilename: '[name]/main.js.map',
     path: path.join(__dirname, '..', 'build')
   },
   plugins: [
-    new webpack.optimize.UglifyJsPlugin(),  
+    // new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.CommonsChunkPlugin({ name: 'clientlibs', filename: 'client/libs.js', chunks: ['client'] })
   ],
   resolve: {
     extensions: ['', '.js', '.jsx', '.ts', '.tsx', '.json']
--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -5,28 +5,34 @@ module.exports = {
   context: path.join(__dirname, '..'),
   entry: {
     client: './src/client/client.tsx',
-    server: './src/server/server.ts'
+    server: './src/server/server.ts',
+    clientlibs: ['react', 'react-dom', 'normalize-css']
   },
   module: {
     loaders: [
-      { test: /\.tsx?$/, loader: 'expose?app!ts-loader'},
+      { test: /\.tsx?$/, loader: 'babel!ts-loader'},
       { test: /\.html$/, loader: 'file?name=client/[name].html'},
       { test: /\.css$/, loader: 'style!css!cssnext' },
       { test: /\.less$/, loader: 'style!css!less' }
     ]
   },
   output: {
-    filename: '[name]/[name].js',
-    sourceMapFilename: '[name]/[name].js.map',
+    filename: '[name]/main.js',
     path: path.join(__dirname, '..', 'build')
   },
   plugins: [
-    new webpack.optimize.UglifyJsPlugin()
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.CommonsChunkPlugin({ name: 'clientlibs', filename: 'client/libs.js', chunks: ['client'] })
   ],
   resolve: {
     extensions: ['', '.js', '.jsx', '.ts', '.tsx', '.json']
   },
   ts: {
-    configFileName: "config/tsconfig.dev.json"
+    configFileName: "config/tsconfig.prod.json"
   }
 };
