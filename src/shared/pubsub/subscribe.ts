c54e84fdc15166b5178d6252c78f8e593537ae29
Paul Shirley <paulshir@gmail.com>
2015-10-11 00:11:18 +0100

    Create promise for subscibe method as some code will run before subscribe is completed

--- src/shared/pubsub/subscribe.ts
+++ src/shared/pubsub/subscribe.ts
@@ -7,16 +7,24 @@ export class SubscriberBase<T extends contract.IDatabaseEntry> {
 		this.subscribeTo = subscribeTo;
 	}
 	
-	public Subscribe(): void {
-		Meteor.subscribe(this.subscribeTo);
+	public Subscribe(): Promise<any> {
+		return new Promise<any>((resolve, reject) => {
+			var a = Meteor.subscribe(this.subscribeTo, <any>{
+				onReady: () => { resolve(); }
+			});
+		});
 	}
 }
 
 export var counterSubscriber: SubscriberBase<contract.ICounter>;
 	
-export function SubscribeAll() {
+export function SubscribeAll(): Promise<any> {
+	var promises: Array<Promise<any>> = [];
+		
 	if (!counterSubscriber) {
 		counterSubscriber = new SubscriberBase<contract.ICounter>(contract.Counter.PUBLISH_NAME);
-		counterSubscriber.Subscribe();
+		promises.push(counterSubscriber.Subscribe());
 	}
+	
+	return Promise.all<any>(promises);
 }
